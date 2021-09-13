e0229f12560d3a0ea873708060707cee440242e2
AlanFil <maildoalana@gmail.com>
2021-09-13 15:28:49 +0200

    obsługa błędu: brak produktu na stronie

--- main.py
+++ main.py
@@ -18,13 +18,15 @@ def get_input_data():
     return huzaro_input
 
 
-def write_error(error_mes):
+def write_error(error_mes, close=False):
     print(error_mes)
     with open('error.txt', 'a', encoding='utf-8') as f:
         f.write(error_mes)
 
-    print('Okno zostanie zamknięte w ciągu 15 sekund...')
-    sleep(15)
+    if close:
+        print('Okno zostanie zamknięte w ciągu 15 sekund...')
+        sleep(15)
+        sys.exit()
 
 
 def split_data(data_):
@@ -37,8 +39,7 @@ def split_data(data_):
             continue
 
         if len(data_[i]) != 2:
-            write_error(f'Wystąpił błąd w linijce {i}\n')
-            sys.exit()
+            write_error(f'Wystąpił błąd w linijce {i}\n', close=True)
 
     data_ = list(filter(lambda x: x != [''], data_))  # remove empty elements
 
@@ -50,21 +51,31 @@ def main(data):
     worksheet = workbook.add_sheet('Sheet 1')
 
     for i, (ean, link) in enumerate(tqdm(data)):
-        sel = Selector(text=requests.get(link.strip()).content)
-        price = sel.xpath('//em[contains(@class, "main-price")]//text()').extract()[0]
-        price = float(price.replace('\xa0', '').replace('zł', '').replace(',', '.'))
-        price = price - (price * 0.15)
-        price = format(price, '.2f')
+        link = link.strip()
+        ean = ean.strip()
+        sel = Selector(text=requests.get(link).content)
+        try:
+            price = sel.xpath('//em[contains(@class, "main-price")]//text()').extract()[0]
+            price = float(price.replace('\xa0', '').replace('zł', '').replace(',', '.'))
+            price = price - (price * 0.15)
+            price = format(price, '.2f')
+        except IndexError:
+            price = sel.xpath('//div[@class="alert-error alert"]/p//text()').extract()[0]
+
         name = ''.join(sel.xpath('//h1[@class="name"]//text()').extract()).strip()
 
-        try:
-            stock = sel.xpath('//div[@class="delivery"]/span[@class="second"]//text()').extract()[0]
-        except IndexError:
-            stock = sel.xpath('//div[@class="row availability"]/span[@class="second"]//text()').extract()[0]
+        if not price == 'Ten produkt jest niedostępny.':
+            try:
+                stock = sel.xpath('//div[@class="delivery"]/span[@class="second"]//text()').extract()[0]
+            except IndexError:
+                stock = sel.xpath('//div[@class="row availability"]/span[@class="second"]//text()').extract()[0]
+        else:
+            stock = 0
+            write_error(f'\nBrak produktu ({i}) {ean} -- {link}')
 
         stock_val = 1 if stock == "Natychmiast" else 0
 
-        worksheet.write(i, 0, ean.strip())
+        worksheet.write(i, 0, ean)
         worksheet.write(i, 1, price)
         worksheet.write(i, 2, stock_val)
         worksheet.write(i, 3, name)
